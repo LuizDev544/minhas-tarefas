4034f7652106b7dcc1a08ecf3eebdeffffbf022d
LuizDev544 <luizfernando.carrupt@gmail.com>
2025-08-18 14:49:43 -0300

    Criando a Barra Lateral

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,5 +1,20 @@
-const BarraLateral = () => {
-  return <aside>Barra Lateral</aside>
-}
+import FiltroCard from '../../components/FiltroCard'
+import * as S from './styles'
+
+const BarraLateral = () => (
+  <S.Aside>
+    <div>
+      <S.Campo type="text" placeholder="Buscar" />
+      <S.Filtros>
+        <FiltroCard ativo={false} />
+        <FiltroCard ativo={false} />
+        <FiltroCard ativo={false} />
+        <FiltroCard ativo={false} />
+        <FiltroCard ativo={false} />
+        <FiltroCard ativo={true} />
+      </S.Filtros>
+    </div>
+  </S.Aside>
+)
 
 export default BarraLateral
