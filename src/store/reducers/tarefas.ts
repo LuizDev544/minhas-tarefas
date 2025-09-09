c9a4c6cd003a4223acaaeacb69e3b0312c94e2a5
LuizDev544 <luizfernando.carrupt@gmail.com>
2025-09-09 16:37:10 -0300

    Realmente terminando o Projeto 5

--- src/store/reducers/tarefas.ts
+++ src/store/reducers/tarefas.ts
@@ -7,29 +7,7 @@ type TarefasState = {
 }
 
 const initialState: TarefasState = {
-  itens: [
-    {
-      id: 1,
-      descricao: 'Estudar Javascript',
-      prioridade: enums.Prioridade.NORMAL,
-      status: enums.Status.CONCLUIDA,
-      titulo: 'Bora esudar'
-    },
-    {
-      id: 2,
-      descricao: 'Estudar Quimica',
-      prioridade: enums.Prioridade.NORMAL,
-      status: enums.Status.PENDENTE,
-      titulo: 'Bora esudar dnv rsrs'
-    },
-    {
-      id: 3,
-      descricao: 'Estudar HTML',
-      prioridade: enums.Prioridade.IMPORTANTE,
-      status: enums.Status.PENDENTE,
-      titulo: 'Bora esudar front :)'
-    }
-  ]
+  itens: []
 }
 
 const tarefaSlice = createSlice({
